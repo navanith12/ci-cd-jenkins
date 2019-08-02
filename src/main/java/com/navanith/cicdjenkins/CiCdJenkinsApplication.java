90504495cce58b5a7395a3daf0aab20da6f6d435
navanith12 <navanithrao337@gmail.com>
2019-08-02 11:55:45 -0400

    example

---
+++ src/main/java/com/navanith/cicdjenkins/CiCdJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.navanith.cicdjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdJenkinsApplication.class, args);
+	}
+
+}
